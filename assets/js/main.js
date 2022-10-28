a6a1b42f4c36b6dd7d5400436a7e42f2ad14c172
snerva <selenenerva@live.it>
2022-10-28 18:52:21 +0200

    task xmark remove

--- assets/js/main.js
+++ assets/js/main.js
@@ -2,7 +2,7 @@
 //MILESTONE 1
 //Stampare all'interno di una lista, un item per ogni todo. Se la proprietà done è uguale a true, visualizzare il testo del todo sbarrato.
 //MILESTONE 2
-//Visualizzare a fianco ad ogni item ha una "x": cliccando su di essa, il todo viene rimosso dalla lista.
+//Visualizzare a fianco ad ogni item una "x": cliccando su di essa, il todo viene rimosso dalla lista.
 //MILESTONE 3
 //Predisporre un campo di input testuale e un pulsante "aggiungi": cliccando sul pulsante, il testo digitato viene letto e utilizzato per creare un nuovo todo, che quindi viene aggiunto alla lista dei todo esistenti.
 //Bonus:
@@ -29,6 +29,11 @@ createApp({
                     done: false
                 },
             ]
+        }
+    },
+    methods: {
+        taskDone(index){
+            this.tasks.splice(index, 1)
         }
     }
    
